bf3168aca424bd5f237e11ffda5ce95313b83947
Marc Lowenstein <marc.lowenstein@gmail.com>
2016-04-17 21:39:54 +0200

    Add project files.

---
+++ DutchHearts/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DutchHearts.MvcApplication" Language="C#" %>
---
+++ DutchHearts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DutchHearts.Startup))]
+namespace DutchHearts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
